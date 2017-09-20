72021a4b96c27965bfc5ea46d8bd02e5d74881c4
Marya Sawaf <sawafmarya@gmail.com>
2017-09-20 12:07:35 -0400

    Update userContextController.js
    Changes recommended by bphillips

--- userContextController.js
+++ userContextController.js
@@ -1,38 +1,28 @@
 ({
-    onInterfaceContentLoaded: function(component, event, helper) {
-        var userTitle;
-     
-        var action = component.get('c.getUserTitle');
-        
-        action.setCallback(this, function(response){
-                if(response.getState() == 'SUCCESS') {
-                    userTitle = response.getReturnValue();
-                }
-            })
-            $A.enqueueAction(action);
-        
-  	 	Coveo.$('.CoveoSearchInterface').on('buildingQuery', function(e, args) {
-           
-            	//add pizza to query
-                //args.queryBuilder.expression.add('pizza');
-            
-            	//add user context to query
-         		args.queryBuilder.context = {};
-            	console.log(userTitle);
-                args.queryBuilder.context.userTitle = userTitle;              
-        });
-    },
     generateSearchToken: function(component, event, helper) {
+            console.log('**** in generateSearchToken');
+            var userRegion;
             var deferred = event.getParam('deferred');        
             var action = component.get('c.getToken');
-        	console.log('hello');
-     
-            action.setCallback(this, function(response){
+
+            action.setCallback(this, function(response){                
                 if(response.getState() == 'SUCCESS') {
-                    component.set("v.usertitle", response.getReturnValue()[1]);
+                    userTitle = response.getReturnValue()[1];
+                    component.set("v.usertitle", userTitle);                    
                     deferred.resolve({searchToken: response.getReturnValue()[0]});
-                }
+                      // Set the Context to pass values to point-and-click Pipeline rules in Coveo Admin tool      
+                    Coveo.$('.CoveoSearchInterface').on('buildingQuery', function(e, args) {            
+                         args.queryBuilder.context = {};
+                        console.log("**** setting Coveo args.queryBuilder.context to " + userTitle);
+                        args.queryBuilder.context.userTitle = userTitle;              
+                    });         
+                                      
+                } else {
+                    console.log("Failed with state: " + state);
+                    console.log("Return value: " + response.getReturnValue());                    
+                };
             })
-            $A.enqueueAction(action);
-        }
+            
+            $A.enqueueAction(action);        
+        }    
 })
